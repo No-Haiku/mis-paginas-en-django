35b38635a2882f2ffb9e90e591eb38748a3732a8
No-Haiku <66711328+No-Haiku@users.noreply.github.com>
2021-02-06 01:04:19 -0300

    blog add

--- webempresa/webempresa/blog/admin.py
+++ webempresa/webempresa/blog/admin.py
@@ -6,9 +6,20 @@ from .models import Category,Post
 class CategoryAdmin(admin.ModelAdmin):
     readonly_fields=('created','updated')
 
-admin.site.register(Category, CategoryAdmin)
+
 
 class PostAdmin(admin.ModelAdmin):
     readonly_fields=('created','updated')
+    list_display=('title','author','published','post__categories')
+    ordering=('author','published')
+    search_fields=('title','context','author__username','categories__name')
+    date_hierarchy='published'
+    list_filter=('author__username','categories__name')
+    
+    def post__categories(self, obj):
+
+        return ", ".join([c.name for c in obj.categories.all().order_by("name")])
+    post__categories.short_description="Categorias"
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
